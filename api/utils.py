8ba923f5c312ea257a59a148a08cc729889a01ca
PMwaniki <PMwaniki@kemri-wellcome.org>
2018-03-03 13:59:49 +0300

    changes to util file

--- api/utils.py
+++ api/utils.py
@@ -14,7 +14,8 @@ def get_errors(request):
         stop=stop[0]
     validation=Validation.objects.get(validation_id=validation_id)
     script=validation.script
-    error_module=importlib.import_module(f"api.error_scripts.{script}")
+    error_module=importlib.import_module('api.error_scripts.%s' % script)
+
     data=error_module.generate(start=start,stop=stop)
     now=datetime.now(tz=pytz.timezone("Africa/Nairobi"))
     validation.last_updated=now
