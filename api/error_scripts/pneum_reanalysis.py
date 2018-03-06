0b9a8b5fd4881f8141144ec932a29d0fab2564fe
PMwaniki <PMwaniki@kemri-wellcome.org>
2018-03-06 11:46:38 +0300

    modify pneumonia reanalysis script

--- api/error_scripts/pneum_reanalysis.py
+++ api/error_scripts/pneum_reanalysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from verify.settings import BASE_DIR
+import os
 
 def generate(start=None,stop=None):
-    data = pd.read_csv("api/tmp/Records with weight or antibiotics documentation issues.csv")
+    data = pd.read_csv(os.path.join(BASE_DIR, "api/tmp/Records with weight or antibiotics documentation issues.csv"))
     return data
