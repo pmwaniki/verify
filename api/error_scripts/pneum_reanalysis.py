359282c08e5e2c580a5927192be9e5b580dbea70
PMwaniki <PMwaniki@kemri-wellcome.org>
2018-03-06 11:27:41 +0300

    modified pneumonia reanalysis script

--- api/error_scripts/pneum_reanalysis.py
+++ api/error_scripts/pneum_reanalysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def generate(start=None,stop=None):
-    data = pd.read_csv("/home/pmwaniki/Dropbox/others/Grace/Feedback Intervention/Records with weight or antibiotics documentation issues.csv")
+    data = pd.read_csv("api/tmp/Records with weight or antibiotics documentation issues.csv")
     return data
