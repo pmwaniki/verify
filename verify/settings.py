342e8c792ebe4a7464a5f255ed595432b840523a
PMwaniki <PMwaniki@kemri-wellcome.org>
2018-04-13 16:06:32 +0300

    Changed backend to mysql

--- verify/settings.py
+++ verify/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'verify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+        }
+
     }
 }
 
