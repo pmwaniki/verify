49120a5f6428149c4604e5c31182b0b450b972c2
PMwaniki <PMwaniki@kemri-wellcome.org>
2018-04-16 12:46:08 +0300

    Modified admin to display models as tables

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from api.models import Validation, Image,History,Hospital
 # Register your models here.
 
-admin.site.register(Validation)
-admin.site.register(Image)
-admin.site.register(History)
-admin.site.register(Hospital)
+class ValidationAdmin(admin.ModelAdmin):
+    list_display=('validation_id' ,'name', 'field_unique' ,'script', 'mapping')
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display=('user' ,'hosp_id', 'record_id' ,'ipno', 'date_created')
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display=('validation' ,'values')
+
+class HospitalAdmin(admin.ModelAdmin):
+    list_display=('id' ,'name')
+
+admin.site.register(Validation,ValidationAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(History,HistoryAdmin)
+admin.site.register(Hospital, HospitalAdmin)
